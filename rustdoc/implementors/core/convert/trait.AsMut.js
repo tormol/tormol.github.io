8d96aabdde8e142f08df10ac55c0a509ecec7927
Torbjørn Birch Moltu <t.b.moltu@lyse.net>
2017-06-16 11:51:01 +0200

    Update rustdoc for encode_unicode 0.3.1

--- rustdoc/implementors/core/convert/trait.AsMut.js
+++ rustdoc/implementors/core/convert/trait.AsMut.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["ascii"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.AsMut.html' title='trait core::convert::AsMut'>AsMut</a>&lt;[<a class='enum' href='ascii/enum.AsciiChar.html' title='enum ascii::AsciiChar'>AsciiChar</a>]&gt; for <a class='struct' href='ascii/struct.AsciiStr.html' title='struct ascii::AsciiStr'>AsciiStr</a>",];
+implementors["ascii"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsMut.html\" title=\"trait core::convert::AsMut\">AsMut</a>&lt;[<a class=\"enum\" href=\"ascii/enum.AsciiChar.html\" title=\"enum ascii::AsciiChar\">AsciiChar</a>]&gt; for <a class=\"struct\" href=\"ascii/struct.AsciiStr.html\" title=\"struct ascii::AsciiStr\">AsciiStr</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
